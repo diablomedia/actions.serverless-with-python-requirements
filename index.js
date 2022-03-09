c06fafafe2bcd710fb3cff1c423f7d1abcf3b602
Alex Albright <aalbright@diablomedia.com>
2022-03-09 14:35:05 -0700

    adding thing back

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var exeq = require('exeq')
 
 //  Input variables
 var ARGS = core.getInput('args')
-var DIR = core.getInput('dir')
+var DIRECTORY = core.getInput('dir')
 
 //  Installs Serverless and specified plugins
 async function installServerlessAndPlugins() {
@@ -21,6 +21,7 @@ async function installServerlessAndPlugins() {
 //  Runs Serverless deploy using SERVERLESS_ACCESS_KEY if specified, else AWS Credentials
 async function runServerlessDeploy() {
   await exeq(
+    `echo Directory is set to: ${DIRECTORY}...`,
     `echo Running sls deploy ${ARGS}...`,
     `if [ ${process.env.AWS_ACCESS_KEY_ID} ] && [ ${process.env.AWS_SECRET_ACCESS_KEY} ]; then
       sls config credentials --provider aws --key ${process.env.AWS_ACCESS_KEY_ID} --secret ${process.env.AWS_SECRET_ACCESS_KEY} ${ARGS}
